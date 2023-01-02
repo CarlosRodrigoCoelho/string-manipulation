7f43975975e57845bfce91d60bfe31b3763e4f26
Carlos Coelho <80289056+CarlosRodrigoCoelho@users.noreply.github.com>
2023-01-02 17:06:16 -0300

    Update extrator_zip.py

--- extrator_zip.py
+++ extrator_zip.py
@@ -4,10 +4,10 @@ address = "Av. João Naves de Ávila, 1331 - Tibery, Uberlândia - MG, 38408902"
 # Regular Expression -- RegEx
 import re
 
-# padrao do CEP: 5 digitos + hifen (opcional) + 3 digitos
+# zip code standart: 5 digits + hifen (opcional) + 3 digits
 
 p = re.compile("[0-9]{5}[-]{0,1}[0-9]{3}")
-s = p.search(address) # ira retornar o Match
+s = p.search(address) # will return Match
 if s:
-    zip_code = s.group() # retorna a string padrao
+    zip_code = s.group() #return the default string
     print(zip_code)
